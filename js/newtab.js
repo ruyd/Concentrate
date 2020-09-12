a0f361525b63e7fd7163316091c19031368776b8
RD <ruydelgado@gmail.com>
2020-09-11 20:08:02 -0500

    hover clock

--- js/newtab.js
+++ js/newtab.js
@@ -8,8 +8,22 @@ function toggleFullScreen() {
     }
 }
 
-document.documentElement.addEventListener("click", function(e) {
-  toggleFullScreen();    
-}, false);
-  
- 
+document.documentElement.addEventListener("click", toggleFullScreen, false);
+
+function startTime() {
+  document.getElementById('txt').innerHTML = formatAMPM(new Date());
+  var t = setTimeout(startTime, 500);
+}
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; 
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+
+document.body.onload = () => startTime();
