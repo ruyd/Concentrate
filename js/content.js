a57d0da1e028ccc5ca632963c7a46906f4782a44
RD <ruydelgado@gmail.com>
2020-09-18 14:59:52 -0500

    YouTube

--- js/content.js
+++ js/content.js
@@ -21,7 +21,11 @@ function bind() {
 }
 
 function unbind() {
-  document.documentElement.removeEventListener("dblclick", toggleFullScreen, false);
+  document.documentElement.removeEventListener(
+    "dblclick",
+    toggleFullScreen,
+    false
+  );
 }
 
 //Listener
@@ -35,5 +39,45 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 //Initial State
 bind();
 
-//CBMUTE
-//YTMUTE
+/// FORK
+
+//YouTube Quiet Down
+
+function removeAds() {
+  let strip = ["video-ads ytp-ad-module"];
+  let elements = document.getElementsByClassName("video-ads");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].remove();
+  }
+}
+
+var didWeMute = false; 
+function muteAds() { 
+  let btns = document.getElementsByClassName("ytp-mute-button");
+  let btn = btns.length > 0 ? btns[0] : null;
+  let muted = btn ? btn.getAttribute("aria-label").toLowerCase().indexOf("unmute") > -1 : false;
+  let showing = document.getElementsByClassName("ad-showing").length > 0;//ad-interrupting  
+  
+  if (!showing && didWeMute) {
+    console.log('Content back -> unmuting');
+    btn.click();
+    didWeMute = false;
+  } 
+
+  if (showing && !muted) {
+    console.log('muting ad');
+    btn.click();
+    didWeMute = true; 
+  }  
+}
+
+function startTime() {
+  removeAds();
+  muteAds();
+  var t = setTimeout(startTime, 5000);
+}
+
+setTimeout(startTime, 1000);
+
+//CB Mute
+
